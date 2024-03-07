72ef4706ab4379e3eb9dbb53fd37c3fd985f471f
Andreas Bellmann <andreas@bellmann-online.net>
2024-03-07 14:22:20 +0100

    tiltfile requires yaml output

--- config/install-strimzi-operator.sh
+++ config/install-strimzi-operator.sh
@@ -5,7 +5,7 @@ if [ "$0" != "$SCRIPT_PATH" ] && [ "$SCRIPT_PATH" != "" ]; then
 fi
 
 git checkout strimzi-0.39.0
-kubectl create namespace kafka
+kubectl create namespace kafka -o yaml
 sed -i 's/namespace: .*/namespace: kafka/' strimzi-0.39.0/install/cluster-operator/*RoleBinding*.yaml
 
 kubectl create -f strimzi-0.39.0/install/cluster-operator -n kafka -o yaml
