2f879f987cdf1e11a9061d60a1487c3af920a09d
Andreas Bellmann <andreas@bellmann-online.net>
2024-03-08 15:42:01 +0100

    output yaml for all commands

--- config/install-knative-serving.sh
+++ config/install-knative-serving.sh
@@ -14,13 +14,15 @@ curl -sSL https://github.com/knative/serving/releases/download/knative-v1.10.1/s
       --certificate-oidc-issuer=https://accounts.google.com
 
 # install knative serving crds
-kubectl apply -f ./knative--1.13.1/install/serving-crds.yaml -o yaml
+kubectl apply -f ./knative-1.13.1/install/serving-crds.yaml -o yaml
 # install knative serving core components
-kubectl apply -f ./knative-1.13.1/install/serving-core.yaml
+kubectl apply -f ./knative-1.13.1/install/serving-core.yaml -o yaml
 # install networking layer
-kubectl apply -f ./knative-1.13.1/install/kourier.yaml
+kubectl apply -f ./knative-1.13.1/install/kourier.yaml -o yaml
 # configure knative to use kourier networking
 kubectl patch configmap/config-network \
   --namespace knative-serving \
   --type merge \
+  --output='yaml' \
   --patch '{"data":{"ingress-class":"kourier.ingress.networking.knative.dev"}}'
+
